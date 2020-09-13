19165f508641a94973f5de45a88d184085c150c8
Avanesov Philipp <floppyrip@gmail.com>
2020-09-13 20:21:48 +0300

    Updates

---
+++ precache-manifest.451fe3dbbadb26011198f24378d82c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d971bb86603de62588f7608ff901d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7625e97039c21d672241",
+    "url": "/static/css/main.d1eaf7a3.chunk.css"
+  },
+  {
+    "revision": "2a81c81b2a4d5cf63929",
+    "url": "/static/js/2.91101185.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91101185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7625e97039c21d672241",
+    "url": "/static/js/main.186d089b.chunk.js"
+  },
+  {
+    "revision": "8c522a916ae06bd75a5b",
+    "url": "/static/js/runtime-main.0935e7e7.js"
+  }
+]);
